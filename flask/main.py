7c3259c0e4c43fe26ac51b339d279442545e1f95
东曦既驾 <future007s@outlook.com>
2025-05-12 10:06:38 +0800

    更新主页消息为 "Hello, Flask11!"

--- flask/main.py
+++ flask/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"message": "Hello, Flask!"})
+    return jsonify({"message": "Hello, Flask11!"})
 
 
 if __name__ == "__main__":
